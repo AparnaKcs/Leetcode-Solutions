36e7c0a259a8f99157ddb9c91077152539dc6f3a
APARNA K <137692953+AparnaKcs@users.noreply.github.com>
2026-08-12 11:06:16 +0530

    Create 520-DetectCapital

---
+++ 520-DetectCapital/520-DetectCapital.java
@@ -0,0 +1,20 @@
+// Last updated: 8/12/2026, 11:06:15 AM
+class Solution {
+    public boolean detectCapitalUse(String word) {
+
+        if (word.equals(word.toUpperCase())) {
+            return true;
+        }
+
+        if (word.equals(word.toLowerCase())) {
+            return true;
+        }
+
+        if (Character.isUpperCase(word.charAt(0)) &&
+            word.substring(1).equals(word.substring(1).toLowerCase())) {
+            return true;
+        }
+
+        return false;
+    }
+}
