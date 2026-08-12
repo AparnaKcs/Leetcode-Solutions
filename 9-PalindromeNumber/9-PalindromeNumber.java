f6fbd9362e3cf2de0d90e7d2144ebf9261c94540
APARNA K <137692953+AparnaKcs@users.noreply.github.com>
2026-08-12 11:09:39 +0530

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,14 @@
+// Last updated: 8/12/2026, 11:09:38 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+}
